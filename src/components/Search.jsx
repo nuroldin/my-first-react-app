cf0554fc4c9a7a6f9e670843030babf12c017b3a
nuroldin <nuroldin.umpara.pimping08@gmail.com>
2025-06-30 16:41:54 +0800

    feat: initialize project with React, Tailwind CSS, and essential components
    - Added main application structure in App.jsx
    - Implemented Search component for movie searching functionality
    - Included SVG assets for search and star icons
    - Created CSS styles for layout and design using Tailwind CSS
    - Set up Vite configuration for React and Tailwind CSS integration
    - Added images for hero background, logo, and no movie found state

---
+++ src/components/Search.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const person = {
+  name: 'Bruce Wayne',
+  age: 36,
+  location: 'Gotham City',
+}
+
+const { name, age, location } = person;
+
+console.log(name); // Bruce Wayne
+
+const Search = ({ searchTerm, setSearchTerm}) => {
+  return (
+    <div className='search'>
+      <div>
+        <img src="search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Search;
